51f37fb595eeb211edd8129bac4e8365a6dc5eb6
Volha Trehubava <neolelya@gmail.com>
2019-06-18 19:38:49 +0200

    Проработана кроссбаузерность

--- js/script.js
+++ js/script.js
@@ -26,6 +26,7 @@ window.addEventListener("keydown", function (evt) {
     if (popup.classList.contains("modal-show")) {
       evt.preventDefault();
       popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
   }
 });
@@ -33,6 +34,8 @@ window.addEventListener("keydown", function (evt) {
 form.addEventListener("submit", function (evt) {
   if (!login.value || !email.value || !message.value) {
     evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
   }
 });
